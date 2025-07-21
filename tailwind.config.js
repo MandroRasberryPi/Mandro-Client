b2d69a8c01d84aae6a6bd2053e9f1fdd6eed3054
psm1st <psm1st@sookmyung.ac.kr>
2025-07-21 13:54:15 +0900

    chore : tailwind 테마 세팅

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,31 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#1E3A8A',
+          light: '#3B82F6',
+          softer: '#60A5FA',
+        },
+        gray: {
+          border: '#D1D5DB',
+          track: '#E5E7EB',
+          placeholder: '#9CA3AF',
+        },
+      },
+      spacing: {
+        '96': '24rem',
+      },
+      width: {
+        '44p': '44%',
+        '48p': '48%',
+      },
+      height: {
+        '16': '4rem',
+        '10': '2.5rem',
+      },
+    },
   },
   plugins: [],
 }
